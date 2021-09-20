6c13e41f009ee8a27112e27d4d1c6677ddda0f29
Marble <59565280+marbleworks@users.noreply.github.com>
2021-09-20 09:43:22 +0900

    Completed

--- bool8.cs
+++ bool8.cs
@@ -1,38 +1,44 @@
+using System;
+
 namespace Marble
 {
-  public struct bool8
-  {
-    private byte _num;
-    
-    public bool Get(int index)
+    public struct bool8
     {
-      var i = 1 << index;
-      return (_num & i) != 0;
-    }
+        private byte _num;
     
-    public bool Set(int index, bool value)
-    {
-      var i = 1 << index;
-      if (value)
-      {
-        _num = (byte)(_num | i);
-      }
-      else
-      {
-        _num = (byte)(_num & ~i);
-      }
+        public bool Get(int index)
+        {
+            var i = 1 << index;
+            return (_num & i) != 0;
+        }
+    
+        public void Set(int index, bool value)
+        {
+            var i = 1 << index;
+            if (value)
+            {
+                _num = (byte)(_num | i);
+            }
+            else
+            {
+                _num = (byte)(_num & ~i);
+            }
+        }
       
-    }
-      
-    public void Toggle(int index)
-    {
-        var i = 1 << index;
-        _num = (byte)(_num ^ i);
-    }
+        public void Toggle(int index)
+        {
+            var i = 1 << index;
+            _num = (byte)(_num ^ i);
+        }
     
-    public void ToggleAll()
-    {
-      _num = (byte)(~num);
+        public void ToggleAll()
+        {
+            _num = (byte)~_num;
+        }
+
+        public void Debug()
+        {
+            UnityEngine.Debug.Log(Convert.ToString(_num, 2));
+        }
     }
-  }
 }
